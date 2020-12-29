003ab38bb4d2a73e468a6813eb7aa61ccda6716c
libre-land <steadstamina@gmail.com>
2020-12-29 00:35:53 -0800

    added functions

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 class LoginScreen(Screen):
-    def next_slide_button(self):
+    def next_slide_button_1(self):
         self.ids.slide.load_next()
+        self.ids.progress_bar_1.value = 100
 
 
-    def previous_slide_button(self):
+    def next_slide_button_2(self):
+        self.ids.slide.load_next()
+        self.ids.progress_bar_2.value = 100
+
+
+    def previous_slide_button_1(self):
         self.ids.slide.load_previous()
+        self.ids.progress_bar_1.value = 0
+
+
+    def previous_slide_button_2(self):
+        self.ids.slide.load_previous()
+        self.ids.progress_bar_2.value = 0
 
 
 class MyMessengerApp(MDApp):
