13e882f22e1dbe59f9e6c399e5517cdcb3c05361
libre-land <steadstamina@gmail.com>
2020-12-29 03:51:14 -0800

    fixed SignUp progress

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ class LoginScreen(Screen):
         self.ids.icon_2.text_color = self.theme_cls.primary_color
 
 
+    def join_button(self):
+        self.theme_cls = ThemeManager()
+        self.ids.icon_3.text_color = self.theme_cls.primary_color
+
+
     def previous_slide_button_1(self):
         self.ids.slide.load_previous()
         self.ids.progress_bar_1.value = 0
@@ -29,9 +34,15 @@ class LoginScreen(Screen):
 
     def previous_slide_button_2(self):
         self.ids.slide.load_previous()
-        self.ids.progress_bar_2.value = 0
+        self.ids.progress_bar_1.value = 0
         self.ids.icon_2.text_color = 0, 0, 0, 1
-        
+
+
+    def previous_slide_button_3(self):
+        self.ids.slide.load_previous()
+        self.ids.progress_bar_2.value = 0
+        self.ids.icon_3.text_color = 0, 0, 0, 1
+
 
 class MyMessengerApp(MDApp):
     def build(self):
